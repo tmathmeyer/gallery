38628d5f89e3c9df60809434b5ab4b36a57df338
Ted Meyer <tmathmeyer@gmail.com>
2017-02-08 00:15:21 -0800

    panoramics

--- server.go
+++ server.go
@@ -29,6 +29,11 @@ type Image struct {
 	Desc string
 }
 
+type Pano struct {
+	Uri string
+	HikeID string
+}
+
 type HikeDetail struct {
 	HikeName string
 	HikeID   string
@@ -85,7 +90,7 @@ func hikeDetailhandler(w http.ResponseWriter, r *http.Request) {
 				Panos:    Panos,
 				Images:   Images}
 
-			t, _ := template.ParseFiles("detail.template.html")
+			t, _ := template.ParseFiles("templates/detail.html")
 			t.Execute(w, Detail)
 			return
 		}
@@ -140,7 +145,7 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("index.template.html")
+	t, _ := template.ParseFiles("templates/index.html")
 	var config Config
 	if _, err := toml.DecodeFile("config.toml", &config); err != nil {
 		fmt.Printf("%s\n", err)
@@ -158,6 +163,57 @@ func exists(path string) bool {
 		return false
 	}
 	return true
+}
+
+func panoramicHandler(w http.ResponseWriter, r *http.Request) {
+	typepath := strings.SplitN(r.URL.Path[len("/img/"):], "/", 3)
+	if len(typepath) != 3 {
+		http.NotFound(w, r)
+		return
+	}
+
+	if strings.HasPrefix(typepath[0], ".") {
+		http.NotFound(w, r)
+		fmt.Printf("%s : [%s]\n", "Cannot start with a '.'", typepath[0])
+		return
+	}
+	if strings.HasPrefix(typepath[1], ".") {
+		http.NotFound(w, r)
+		fmt.Printf("%s : [%s]\n", "Cannot start with a '.'", typepath[1])
+		return
+	}
+
+	if typepath[0] == "raw" {
+		img_path := "./gallerydata/" + typepath[1] + "/pan/" + typepath[2]
+		http.ServeFile(w, r, img_path)
+		return
+	}
+
+	if typepath[0] == "full" {
+		t, _ := template.ParseFiles("templates/pano_view.html")
+        var img = Pano{Uri:  typepath[2],
+			 	       HikeID: typepath[1]}
+		t.Execute(w, img)
+		return
+	}
+
+	if typepath[0] == "small" {
+		img_path := "./gallerydata/" + typepath[1] + "/pan/" + typepath[2]
+		thm_path := "./gallerydata/" + typepath[1] + "/pan/tn_" + typepath[2]
+
+		if !exists(thm_path) {
+			cmd := exec.Command("vipsthumbnail", "-s", "450", img_path)
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+				http.NotFound(w, r)
+				return
+			}
+		}
+
+		http.ServeFile(w, r, thm_path)
+		return
+	}
 }
 
 func hikeDataHandler(w http.ResponseWriter, r *http.Request) {
@@ -192,6 +248,7 @@ func main() {
 	http.HandleFunc("/gallerydetail/", hikeDetailhandler)
 	http.HandleFunc("/gallerydata/", hikeDataHandler)
 	http.HandleFunc("/img/", imageHandler)
+	http.HandleFunc("/pan/", panoramicHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.ListenAndServe(":8081", nil)
 }
