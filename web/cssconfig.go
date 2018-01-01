62648d2e25ea083193fc38ebc00ae8a6b0e4c839
Ted Meyer <tmathmeyer@gmail.com>
2018-01-01 13:25:13 -0800

    changed the default scheme a bit
    The old one is still available by modifying colors.yaml
    next things to do:
     1 : more changes to the css (i hate designing)
     2 : allow real-time modification of the css colors & style from the
     admin panel
     3 : implement photo saving
     4 : re-implement the panoramic viewer
     5 : re-implement the map & gpx viewers
     6 : tbd

--- web/cssconfig.go
+++ web/cssconfig.go
@@ -10,20 +10,23 @@ import (
 )
 
 type CssConfiguration struct {
-	Background			string `yaml:"background"`
-	HeaderBackground	string `yaml:"header_background"`
-	TextColorPress		string `yaml:"text_color_press"`
-	ShadowColorPress	string `yaml:"shadow_color_press"`
-	BreadcrumbColor1	string `yaml:"breadcrumb_color_1"`
-	BreadcrumbText1		string `yaml:"breadcrumb_text_1"`
-	BreadcrumbColor2	string `yaml:"breadcrumb_color_2"`
-	BreadcrumbText2		string `yaml:"breadcrumb_text_2"`
-	BreadcrumbColor3	string `yaml:"breadcrumb_color_3"`
-	BreadcrumbText3		string `yaml:"breadcrumb_text_3"`
-	BreadcrumbColor4	string `yaml:"breadcrumb_color_4"`
-	BreadcrumbText4		string `yaml:"breadcrumb_text_4"`
-	BreadcrumbColor5	string `yaml:"breadcrumb_color_5"`
-	BreadcrumbText5		string `yaml:"breadcrumb_text_5"`
+	Background			string	`yaml:"background"`
+	HeaderBackground	string	`yaml:"header_background"`
+	TextColorPress		string	`yaml:"text_color_press"`
+	ShadowColorPress	string	`yaml:"shadow_color_press"`
+	BreadcrumbColor1	string	`yaml:"breadcrumb_color_1"`
+	BreadcrumbText1		string	`yaml:"breadcrumb_text_1"`
+	BreadcrumbColor2	string	`yaml:"breadcrumb_color_2"`
+	BreadcrumbText2		string	`yaml:"breadcrumb_text_2"`
+	BreadcrumbColor3	string	`yaml:"breadcrumb_color_3"`
+	BreadcrumbText3		string	`yaml:"breadcrumb_text_3"`
+	BreadcrumbColor4	string	`yaml:"breadcrumb_color_4"`
+	BreadcrumbText4		string	`yaml:"breadcrumb_text_4"`
+	BreadcrumbColor5	string	`yaml:"breadcrumb_color_5"`
+	BreadcrumbText5		string	`yaml:"breadcrumb_text_5"`
+
+	PowerlineCrumb		bool	`yaml:"style_crumb"`
+	UnderlineCrumb		bool	`yaml:"style_underline"`
 }
 
 func (c *CssConfiguration) getColors() *CssConfiguration {
@@ -39,9 +42,21 @@ func (c *CssConfiguration) getColors() *CssConfiguration {
     return c
 }
 
+func powerline_crumb() bool {
+	return true
+}
+
 func CssConfigureHandler(db *sql.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("templates/common.css")
+		t, err := template.ParseFiles(
+			"templates/css/common.css",
+			"templates/css/default.css",
+			"templates/css/powerline.css",
+			"templates/css/underline.css",
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
 		var conf CssConfiguration
 		conf.getColors()
 		t.Execute(w, conf)
